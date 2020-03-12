03c59971023d48a7e2fa414366d56e6da6e53a97
Azharuddin Mohammed <46964996+azhar2ds@users.noreply.github.com>
2020-03-12 15:27:41 +0530

    Even Numbers files

---
+++ Even_number.py
@@ -0,0 +1,10 @@
+
+print("this is 4 table")
+count = 0
+for number in range(4, 44):
+    if number % 4 == 0:
+        count += 1
+        print(number)
+print(f" this is {count}")
+
+print("Helloworld")
